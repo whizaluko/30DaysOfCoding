953eb54d471b2cfd7ea1d3549df21efb287c61b5
Blessing Aluko <58727532+whizaluko@users.noreply.github.com>
2020-01-16 13:21:25 +0100

    Update SemicolonExercise.py

--- SemicolonExercise.py
+++ SemicolonExercise.py
@@ -1,23 +1,25 @@
 # Safe Lead in Basketball
-TeamPoints = int(input("Enter the lead team points: "))
+TeamPoints = int(input("Enter the points difference for the leading team: "))
 
-TeamPoints = TeamPoints - 3
+TeamPoints -= 3
 
 TeamAhead = input("Is the ball with the team that is ahead (Yes or No): ")
 
 if TeamAhead == "Yes":
-    TeamPoints = float(TeamPoints + 0.5)
+    TeamPoints += 0.5
 else:
-    TeamPoints = float(TeamPoints - 0.5)
+    TeamPoints -= 0.5
 
 if TeamPoints < 0:
     TeamPoints = 0
 
-TeamPoints = TeamPoints ** 2
+TeamPoints **= 2
 
-SecondsLeft = int(input("Enter the seconds remaining: "))
+SecondsLeft = float(input("Enter the seconds remaining: "))
 
 if TeamPoints > SecondsLeft:
     print("The lead is safe")
 else:
     print("The lead is not safe")
+  
+
